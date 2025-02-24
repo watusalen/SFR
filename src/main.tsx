9de70c688fa667a60bac41ea1dd20cf6c7db85ad
theonealves <matusalencosta@icloud.com>
2025-02-24 14:29:53 -0300

    Changes to index.ts and main.tsx to visualize the debugging of the code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import './domain/simulation-engine/index.ts'
 createRoot(document.getElementById("root")!).render(<App />);
