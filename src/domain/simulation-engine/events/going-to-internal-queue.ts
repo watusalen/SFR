39f3e4f2fc3c4e79af88424de1cfefe2935fe9b3
Matusalen Alves <matusalencosta@icloud.com>
2025-02-24 11:30:45 -0300

    Adding logic to Events

--- src/domain/simulation-engine/events/going-to-internal-queue.ts
+++ src/domain/simulation-engine/events/going-to-internal-queue.ts
@@ -3,6 +3,8 @@ import { EventMachine } from "./event-machine";
 import { Event } from "./event";
 import { GoingToService } from "./going-to-service";
 import { GoingToTurnstile } from "./going-to-turnstile";
+import { UnlockTurnstile } from "./unlock-turnstile";
+import { LockService } from "./lock-service";
 
 export class GoingToInternalQueue extends Event {
 
@@ -15,18 +17,34 @@ export class GoingToInternalQueue extends Event {
         console.log(`Evento - Transição da Catraca para a Fila Interna: ${this.getTimeStamp()}`);
 
         //Alteração do estado do Sistema
-        const sucess: boolean = this.cafeteria.moveStudentFromTurnstileToInternalQueue();
+        this.cafeteria.moveStudentFromTurnstileToInternalQueue();
 
+        //Log
         console.log(`Quantidade de Alunos na Fila Interna: ${this.cafeteria.checkInternalQueueSize()}`);
+
+        //Variáveis para controle e geração de novos Eventos
+        const turnstileIsLocked: boolean = this.cafeteria.checkTurnstileLocked();
+        const hasSomeoneInExternalQueue: boolean = this.cafeteria.hasSomeoneInExternalQueue();
 
         //Variáveis para controle e geração de novos Eventos
         const hasSomeoneInService: boolean = this.cafeteria.hasSomeoneInService();
         const serviceIsLocked: boolean = this.cafeteria.checkServiceLocked();
+        const internalQueueLimitRecheadMaximum: boolean = this.cafeteria.checkInternalQueueLimitRecheadMaximum();
 
         //Possíveis novos Eventos gerados a partir deste Evento
-        if (sucess && !hasSomeoneInService && !serviceIsLocked) {
-            const scheduling: Event = new GoingToService(this.getTimeStamp(), this.cafeteria, this.machine);
-            this.machine.addEvent(scheduling);
+        if (turnstileIsLocked && hasSomeoneInExternalQueue && !internalQueueLimitRecheadMaximum) {
+            const scheduling3: Event = new UnlockTurnstile(this.getTimeStamp(), this.cafeteria, this.machine);
+            const scheduling4: Event = new GoingToTurnstile(this.getTimeStamp(), this.cafeteria, this.machine);
+            this.machine.addEvent(scheduling3);
+            this.machine.addEvent(scheduling4);
+        }
+
+        //Possíveis novos Eventos gerados a partir deste Evento
+        if (!hasSomeoneInService && !serviceIsLocked) {
+            const scheduling1: Event = new GoingToService(this.getTimeStamp(), this.cafeteria, this.machine);
+            const scheduling2: Event = new LockService(this.getTimeStamp(), this.cafeteria, this.machine);
+            this.machine.addEvent(scheduling1);
+            this.machine.addEvent(scheduling2);
         }
     }
 }
